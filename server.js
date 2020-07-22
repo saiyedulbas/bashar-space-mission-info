ab6c31328bf240390b39def3d344ed67bab23d99
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2020-07-22 18:55:09 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const cors = require('cors');
-const schema = require('./schema');
-const path = require('path');
 
-const app = express();
 
-// Allow cross-origin
-app.use(cors());
+        express();
 
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true
-  })
-);
 
-app.use(express.static('public'));
+            app.use(cors())'/graphql',
+        graphql);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+      app.get('*', (req, res) => {
+  res.sendF
+            const app = ;const express = require('express');
+            const graphqlHTTP = require('express-graphql');
+            const cors = require(HTTP({
+                schema,
+                graphiql: true
+              })
+            );
+            app.use(ile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+        app.use(express.static('public')'cors');
+        const schema = require('./schema');
+        const path = require('path');
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+                    const PORT = process.env.PORT || 5000;
+
+                    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
